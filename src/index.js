460ca4867753876031b6bdcf066ab37c2779cb41
tran tan tai <trantantai20081997@gmail.com>
2020-11-04 20:29:02 +0700

    update react-router-dom

--- src/index.js
+++ src/index.js
@@ -9,19 +9,23 @@ axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-// Add a request interceptor
-axios.interceptors.request.use(config => {
-    return config;
-  }, error => {
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // Edit request config
+    return request;
+}, error => {
+    console.log(error);
     return Promise.reject(error);
-  });
+});
 
-// Add a response interceptor
 axios.interceptors.response.use(response => {
+    console.log(response);
+    // Edit request config
     return response;
-  }, error => {
+}, error => {
+    console.log(error);
     return Promise.reject(error);
-  });
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
