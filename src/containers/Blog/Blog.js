2d68404f39a857e18cb84c2abe924be999d61509
tran tan tai <trantantai20081997@gmail.com>
2020-11-05 13:40:14 +0700

    update Blog error

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,7 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
 import asyncComponent from '../../hoc/asyncComponent';
 //import FullPost from './FullPost/FullPost';
 const AsyncNewPost = asyncComponent(() => {
@@ -43,9 +43,9 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                     <Route path="/posts" component={Posts} />
-                    <Route render={()=> <h1>Not Found</h1> } />
+                    <Rout render={()=> <h1>Not Found</h1> } />
                     {/* <Redirect from="/" to="/posts" /> */}
                 </Switch>
                 {/* <Route path="/new-post">
